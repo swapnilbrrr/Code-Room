32382ec09bcee67960f09e305155f34a6b9b6376
swapnil <140897343+swapnilbrrr@users.noreply.github.com>
2026-09-26 00:42:54 +0545

    Add profile activity and notification persistence

--- CodeRoom.Web/Data/ApplicationDbContext.cs
+++ CodeRoom.Web/Data/ApplicationDbContext.cs
@@ -16,12 +16,16 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Progress> Progress => Set<Progress>();
     public DbSet<Resource> Resources => Set<Resource>();
     public DbSet<Announcement> Announcements => Set<Announcement>();
+    public DbSet<UserActivity> UserActivities => Set<UserActivity>();
+    public DbSet<Notification> Notifications => Set<Notification>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
+        modelBuilder.Entity<User>().HasIndex(x => x.Username).IsUnique();
+        modelBuilder.Entity<User>().Property(x => x.Bio).HasMaxLength(500);
 
         modelBuilder.Entity<Enrollment>()
             .HasIndex(x => new { x.UserId, x.CourseId })
@@ -31,10 +35,28 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             .HasIndex(x => new { x.UserId, x.LessonId })
             .IsUnique();
 
+        modelBuilder.Entity<UserActivity>()
+            .HasIndex(x => new { x.UserId, x.CreatedAt });
+
+        modelBuilder.Entity<Notification>()
+            .HasIndex(x => new { x.UserId, x.IsRead, x.CreatedAt });
+
         modelBuilder.Entity<Course>()
             .HasMany(x => x.Lessons)
             .WithOne(x => x.Course)
             .HasForeignKey(x => x.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserActivity>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Activities)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Notification>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Notifications)
+            .HasForeignKey(x => x.UserId)
             .OnDelete(DeleteBehavior.Cascade);
     }
 }
