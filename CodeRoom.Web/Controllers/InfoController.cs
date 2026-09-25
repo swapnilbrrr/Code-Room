bf7b3b803cdefc18e59824bc24f54dddc5de90d3
swapnil <140897343+swapnilbrrr@users.noreply.github.com>
2026-09-25 17:03:19 +0545

    Remove unnecessary Info controller

--- CodeRoom.Web/Controllers/InfoController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace CodeRoom.Web.Controllers;
-
-public class InfoController : Controller
-{
-    public IActionResult About() => View();
-    public IActionResult Faq() => View();
-}
