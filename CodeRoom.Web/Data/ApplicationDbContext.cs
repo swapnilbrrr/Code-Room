4f7412a908eda8a6a47865d5de9e91e220c6dba0
swapnil <140897343+swapnilbrrr@users.noreply.github.com>
2026-09-26 01:31:01 +0545

    Expand ApplicationDbContext.cs for Code-Room 2.0

--- CodeRoom.Web/Data/ApplicationDbContext.cs
+++ CodeRoom.Web/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<Course> Courses => Set<Course>();
+    public DbSet<CourseModule> CourseModules => Set<CourseModule>();
     public DbSet<Lesson> Lessons => Set<Lesson>();
     public DbSet<Enrollment> Enrollments => Set<Enrollment>();
     public DbSet<Quiz> Quizzes => Set<Quiz>();
@@ -18,6 +19,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Announcement> Announcements => Set<Announcement>();
     public DbSet<UserActivity> UserActivities => Set<UserActivity>();
     public DbSet<Notification> Notifications => Set<Notification>();
+    public DbSet<Challenge> Challenges => Set<Challenge>();
+    public DbSet<Achievement> Achievements => Set<Achievement>();
+    public DbSet<UserAchievement> UserAchievements => Set<UserAchievement>();
+    public DbSet<Certificate> Certificates => Set<Certificate>();
+    public DbSet<AdminAuditLog> AdminAuditLogs => Set<AdminAuditLog>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -41,11 +47,46 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.Entity<Notification>()
             .HasIndex(x => new { x.UserId, x.IsRead, x.CreatedAt });
 
+        modelBuilder.Entity<UserAchievement>()
+            .HasIndex(x => new { x.UserId, x.AchievementId })
+            .IsUnique();
+
+        modelBuilder.Entity<Certificate>()
+            .HasIndex(x => x.CertificateNumber)
+            .IsUnique();
+
+        modelBuilder.Entity<AdminAuditLog>()
+            .HasIndex(x => new { x.UserId, x.CreatedAt });
+
         modelBuilder.Entity<Course>()
             .HasMany(x => x.Lessons)
             .WithOne(x => x.Course)
             .HasForeignKey(x => x.CourseId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Course>()
+            .HasMany(x => x.Modules)
+            .WithOne(x => x.Course)
+            .HasForeignKey(x => x.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Lesson>()
+            .HasOne(x => x.CourseModule)
+            .WithMany(x => x.Lessons)
+            .HasForeignKey(x => x.CourseModuleId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Course>()
+            .HasMany(x => x.Challenges)
+            .WithOne(x => x.Course)
+            .HasForeignKey(x => x.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Challenge>()
+            .HasOne(x => x.Lesson)
+            .WithMany(x => x.Challenges)
+            .HasForeignKey(x => x.LessonId)
+            .OnDelete(DeleteBehavior.SetNull);
 
         modelBuilder.Entity<UserActivity>()
             .HasOne(x => x.User)
@@ -56,6 +97,42 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.Entity<Notification>()
             .HasOne(x => x.User)
             .WithMany(x => x.Notifications)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserAchievement>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.UserAchievements)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserAchievement>()
+            .HasOne(x => x.Achievement)
+            .WithMany(x => x.UserAchievements)
+            .HasForeignKey(x => x.AchievementId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Certificate>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Certificates)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Certificate>()
+            .HasOne(x => x.Course)
+            .WithMany(x => x.Certificates)
+            .HasForeignKey(x => x.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Certificate>()
+            .HasOne(x => x.QuizAttempt)
+            .WithMany()
+            .HasForeignKey(x => x.QuizAttemptId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<AdminAuditLog>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.AuditLogs)
             .HasForeignKey(x => x.UserId)
             .OnDelete(DeleteBehavior.Cascade);
     }
