101f9ba9e0ebc79d717a0673afb3ec8ccc4dc902
swapnil <140897343+swapnilbrrr@users.noreply.github.com>
2026-09-26 01:49:32 +0545

    Add SuperAdmin system governance controller

---
+++ CodeRoom.Web/Areas/Admin/Controllers/SystemController.cs
@@ -0,0 +1,24 @@
+using CodeRoom.Web.Data;
+using CodeRoom.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeRoom.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = Roles.SuperAdmin)]
+public class SystemController(ApplicationDbContext db) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.UserCount = await db.Users.CountAsync();
+        ViewBag.AdminCount = await db.Users.CountAsync(u => u.Role == Roles.Admin);
+        ViewBag.SuperAdminCount = await db.Users.CountAsync(u => u.Role == Roles.SuperAdmin);
+        ViewBag.CourseCount = await db.Courses.CountAsync();
+        ViewBag.CertificateCount = await db.Certificates.CountAsync();
+        ViewBag.AuditCount = await db.AdminAuditLogs.CountAsync();
+
+        return View();
+    }
+}
